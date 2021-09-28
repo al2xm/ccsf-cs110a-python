257197b3eb09ef7a39883fce08e0074d9eb399fd
al2xm <89373351+al2xm@users.noreply.github.com>
2021-09-27 18:23:27 -0700

    Create first.py
    first program assignment print hello world and your initials

---
+++ first.py
@@ -0,0 +1,13 @@
+#This program produces simple output
+#Inputs: None
+#Outputs: 3 block letters
+#Written by:Alexander Ma
+#Date: Sep 3,2021
+
+print("Hello, Python!")
+print("Love, ")
+print("""
+   A     M     M    A
+  A A    MM   MM   A A
+ AAAAA   M M M M  AAAAA
+A     A  M  M  M A     A""")
